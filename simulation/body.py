3c2e4c1d7d8cc242d6e0fe8818c01e15fe8ce3ab
Alvaro <alvaro.sanchezmateos@usp.ceu.es>
2025-03-31 18:02:26 +0200

    feat: Movement implemented in Body

--- simulation/body.py
+++ simulation/body.py
@@ -2,12 +2,18 @@ import pygame
 
 class Body:
 
-    def __init__(self, x, y, radius, color, mass):
+    def __init__(self, x, y, radius, color, mass, vx, vy):
         self.x = x # Position (in pixels)
         self.y = y
         self.radius = radius # Radius (in pixels)
         self.color = color
         self.mass = mass
+        self.vx = vx # Velocity (pixels/frame)
+        self.vy = vy
+
+    def movement(self):
+        self.x += self.vx
+        self.y += self.vy
 
     def draw(self, surface):
         pygame.draw.circle(surface, self.color, (self.x, self.y), self.radius)
